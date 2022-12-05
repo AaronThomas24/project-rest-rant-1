1ee751120fe4a5e1bc7419d85f329c89537dc8a4
Aaron Thomas <aaron.thomas231@yahoo.comgit>
2022-12-04 23:27:08 -0800

    added mongo

--- controllers/models/places.js
+++ controllers/models/places.js
@@ -1,16 +1,13 @@
-module.exports = [
-  {
-    name: "H-Thai-ML",
-    city: "Seattle",
-    state: "WA",
-    cuisines: "Thai, Pan-Asian",
-    pic: "/images/h-thai-ml-tables.jpg",
-  },
-  {
-    name: "Coding Cat Cafe",
-    city: "Phoenix",
-    state: "AZ",
-    cuisines: "Coffee, Bakery",
-    pic: "/images/coffee-cat.jpg",
-  },
-];
+const router = require("express").Router();
+const db = require("../models");
+
+router.post("/", (req, res) => {
+  db.Place.create(req.body)
+    .then(() => {
+      res.redirect("/places");
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.render("error404");
+    });
+})((module.exports = mongoose.model("Place", placeSchema)));
--- controllers/places.js
+++ controllers/places.js
@@ -1,52 +1,46 @@
 const router = require("express").Router();
-const places = require("../models/places.js");
-router.put("/:id", (req, res) => {
-  let id = Number(req.params.id);
-  if (isNaN(id)) {
-    res.render("error404");
-  } else if (!places[id]) {
-    res.render("error404");
-  } else {
-    // Dig into req.body and make sure data is valid
-    if (!req.body.pic) {
-      // Default image if one is not provided
-      req.body.pic = "http://placekitten.com/400/400";
-    }
-    if (!req.body.city) {
-      req.body.city = "Anytown";
-    }
-    if (!req.body.state) {
-      req.body.state = "USA";
-    }
 
-    // Save the new data into places[id]
-    places[id] = req.body;
-    res.redirect(`/places/${id}`);
-  }
+router.get("/:id", (req, res) => {
+  db.Place.findById(req.params.id)
+    .then((place) => {
+      res.render("places/show", { place });
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.render("error404");
+    });
 });
 
-// GET /places
-app.get("/", (req, res) => {
-  let places = [
-    {
-      name: "H-Thai-ML",
-      city: "Seattle",
-      state: "WA",
-      cuisines: "Thai, Pan-Asian",
-      pic: "/images/h-thai-ml-tables.png",
-      pic: "/images/thai food.jpg",
-    },
-    {
-      name: "Coding Cat Cafe",
-      city: "Phoenix",
-      state: "AZ",
-      cuisines: "Coffee, Bakery",
-      pic: "/images/coffee-cat.jpg",
-      pic: "/images/espresso.jpg",
-    },
-  ];
+router.post("/", (req, res) => {
+  res.send("POST /places stub");
+});
 
-  res.render("places/show", { place: places[id] });
+router.get("/new", (req, res) => {
+  res.render("places/new");
+});
+
+router.get("/:id", (req, res) => {
+  res.send("GET /places/:id stub");
+});
+
+router.put("/:id", (req, res) => {
+  res.send("PUT /places/:id stub");
+});
+
+router.delete("/:id", (req, res) => {
+  res.send("DELETE /places/:id stub");
+});
+
+router.get("/:id/edit", (req, res) => {
+  res.send("GET edit form stub");
+});
+
+router.post("/:id/rant", (req, res) => {
+  res.send("GET /places/:id/rant stub");
+});
+
+router.delete("/:id/rant/:rantId", (req, res) => {
+  res.send("GET /places/:id/rant/:rantId stub");
 });
 
 module.exports = router;
